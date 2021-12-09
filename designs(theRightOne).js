e387d0c785489f79adaeaf16b37f2ebb3cb6e7c5
CarloDettori <94261894+CarloDettori@users.noreply.github.com>
2021-12-09 15:26:49 +0100

    Add files via upload

---
+++ designs(theRightOne).js
@@ -0,0 +1,36 @@
+const width = document.getElementById('inputWidth').value;
+const height = document.getElementById('inputHeight').value;
+
+function clickCells() {
+     // on color selection return color:
+     const colorPicker = document.getElementById("colorPicker");
+     const cells = document.getElementsByClassName('cell');
+     for (let i = 0; i < cells.length; i++) {
+         cells[i].addEventListener("click", function (event) {
+             let clickedCell = event.target;
+             clickedCell.style.backgroundColor = colorPicker.value;
+         });
+     }
+ }
+
+function makeGrid(height, width) {
+    const table = document.getElementById("pixelCanvas");
+    let grid = '';
+
+    for (let i = 0; i < height; i++) {
+        grid += '<tr class="row-' + i + '">';
+        for (let j = 0; j < width; j++) {
+                grid += '<td class="cell" id="row-' + i + '_cell-' + j + '"></td>';
+            }
+            grid += '</tr>';
+        }
+    table.innerHTML = grid;
+    addClickEventToCells();
+    }
+
+function Submission() {
+    event.preventDefault();
+    makeGrid(height, width);
+
+document.getElementById('sizePicker').onsubmit = function () {
+    Submission();
