cb0d95700b98cd8afb60d6d5f1430a3a0e727f24
CarloDettori <94261894+CarloDettori@users.noreply.github.com>
2021-12-11 18:30:15 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,35 +1,42 @@
-const width = document.getElementById('inputWidth').value;
-const height = document.getElementById('inputHeight').value;
+let width = document.getElementById('inputWidth').value;
+let height = document.getElementById('inputHeight').value;
+const submitt = document.getElementById('submitting').value;
+let desingText = document.getElementById('pixelCanvas').value;
+
 
 function clickCells() {
-     const colorPicker = document.getElementById("colorPicker");
+     const colorPicker = document.getElementById("colorPicker").value;
      const cells = document.getElementsByClassName('cell');
      for (let i = 0; i < cells.length; i++) {
          cells[i].addEventListener("click", function (event) {
              let clickedCell = event.target;
-             clickedCell.style.backgroundColor = colorPicker.value;
+             clickedCell.style.backgroundColor = colorPicker;
          });
      }
  }
 
+
 function makeGrid(height, width) {
     const table = document.getElementById("pixelCanvas");
     let grid = '';
-
     for (let i = 0; i < height; i++) {
-        grid += '<tr class="row-' + i + '">';
         for (let j = 0; j < width; j++) {
-                grid += '<td class="cell" id="row-' + i + '_cell-' + j + '"></td>';
+            let newCell = document.createElement("div");
+            j.appendChild(newCell) = "cell";
             }
-            grid += '</tr>';
         }
     table.innerHTML = grid;
     addClickEventToCells();
     }
+submitt.addEventListener('click', function () {
+    console.log('The heading was clicked!');
 
-function Submission() {
-    event.preventDefault();
-    makeGrid(height, width);
+
+    function Submission() {
+        event.preventDefault();
+        makeGrid(height, width);
+    };
+
 
 document.getElementById('sizePicker').onsubmit = function () {
     Submission();
